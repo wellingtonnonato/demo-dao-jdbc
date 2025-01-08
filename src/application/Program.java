e99967e9bd874dc69fac5d45ec6672070cb20808
wellingtonnoanto <https://github.com/wellingtonnonato>
2025-01-08 18:50:53 -0300

    Criação da classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,38 +8,15 @@ import java.sql.Statement;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = null;
-		Statement st = null;
 		
-		try {
-			conn = DB.getConnection();
-			conn.setAutoCommit(false);
-			
-			st = conn.createStatement();
-			
-			int rows1 = st.executeUpdate("Update seller set baseSalary = 2090 where departmentId = 1");
-			int rows2 = st.executeUpdate("Update seller set baseSalary = 3090 where departmentId = 2");
-			
-			System.out.println("rows1: " + rows1);
-			System.out.println("rows2: " + rows2);
-			conn.commit();
-		}
-		catch(SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbIntegrityException("Transaction rolled back! Caused by: "+ e.getMessage());
-			} catch (SQLException e1) {
-				throw new DbIntegrityException("Error trying to rollback! Caused by: "+ e1.getMessage());
-			}
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}	
+		Department d = new Department(1, "Book");
+		
+		System.out.println(d);
 
 	}
 
