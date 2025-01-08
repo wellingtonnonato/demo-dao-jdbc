b41a4d5aa9b938f9ee176addb0dd28d092ca8845
wellingtonnoanto <https://github.com/wellingtonnonato>
2025-01-08 19:10:03 -0300

    Criação da Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,23 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department d = new Department(1, "Book");
+		Seller seller = new Seller(21
+				, "Bob"
+				, "bob@gmail.com"
+				, new Date()
+				, 3000.0
+				, d);
 		
-		System.out.println(d);
+		System.out.println(seller);
 
 	}
 
