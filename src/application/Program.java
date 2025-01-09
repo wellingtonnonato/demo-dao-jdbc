b6984cb0bce5d4f759c8a4223c4359b54f212b10
wellingtonnoanto <https://github.com/wellingtonnonato>
2025-01-09 19:50:31 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -23,12 +25,23 @@ public class Program {
 	    	System.out.println(obj);
 	    }
 	    
-	    System.out.println("\n=== TEST 3: seller findByDepartmentId =====");
+	    System.out.println("\n=== TEST 3: seller findAll =====");
 	    listSeller = sellerDao.findAll();
 	    
 	    for (Seller obj : listSeller) {
 	    	System.out.println(obj);
 	    }
+	    
+	    System.out.println("\n=== TEST 4: seller insert =====");
+	    Department dep = new Department(2, null);
+	    Seller newSeller = new Seller(null
+	    		, "Greg"
+	    		, "greg@gmail.com"
+	    		, new Date()
+	    		, 4000.0
+	    		, dep);
+	    sellerDao.insert(newSeller);
+	    System.out.println("Inserted! New id = "+ newSeller.getId());
 	}
 
 }
